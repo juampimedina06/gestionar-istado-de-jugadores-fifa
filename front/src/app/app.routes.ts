7f14f532699f467b36a83f7a5957883aeb7a9e88
juampimedina06 <jpmedinagomez1@gmail.com>
2025-10-15 06:57:11 -0300

    creando pantalla inicio

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './features/pages/inicio/inicio';
+import { SubirPlayer } from './features/pages/subir-player/subir-player';
+import { Login } from './features/pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component:Inicio
+    },
+    {
+        path: 'login',
+        component: Login
+    },
+    {
+        path: 'subir-player',
+        component:SubirPlayer
+    }
+];
